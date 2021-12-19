9cd477870b84512700e2e34a5423144d9cf4992c
jjmarchewitz@gmail.com <jjmarchewitz@gmail.com>
2021-12-18 19:42:17 -0500

    Figured out SSH in VSCode

--- tutorial.py
+++ tutorial.py
@@ -6,8 +6,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda, Compose
 
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
 # Define model
@@ -32,7 +31,9 @@ class NeuralNetwork(nn.Module):
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
-    model.train()
+    model.cuda()
+    gap_time = time.time()
+
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
 
@@ -47,13 +48,13 @@ def train(dataloader, model, loss_fn, optimizer):
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
-            # print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
-    model.eval()
+    model.cuda()
     test_loss, correct = 0, 0
     with torch.no_grad():
         for X, y in dataloader:
@@ -63,70 +64,73 @@ def test(dataloader, model, loss_fn):
             correct += (pred.argmax(1) == y).type(torch.float32).sum().item()
     test_loss /= num_batches
     correct /= size
-    # print(
-    #     f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
-    # )
+    print(
+        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
+    )
     return correct, test_loss
 
 
 # Download training data from open datasets.
 training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor(),
+    root="data", train=True, download=True, transform=ToTensor(),
 )
 
 # Download test data from open datasets.
 test_data = datasets.FashionMNIST(
-    root="data",
-    train=False,
-    download=True,
-    transform=ToTensor(),
+    root="data", train=False, download=True, transform=ToTensor(),
 )
+
+# breakpoint()
+
+
 with open("out.txt", "w") as f:
 
     start_time = time.time()
 
-    for batch_size in range(2, 10):
+    batch_size = 128
 
-        # Output batch size
-        print(f"Batch Size: {batch_size} ")
-        f.write(f"Batch Size: {batch_size}")
+    # Output batch size
+    print(f"Batch Size: {batch_size} ")
+    f.write(f"Batch Size: {batch_size}")
 
-        train_dataloader = DataLoader(training_data, batch_size=batch_size)
-        # Create data loaders.
-        test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+    # Create data loaders.
+    test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
-        # Display info about the shape of the data loaders
-        # for X, y in test_dataloader:
-        #     print("Shape of X [N, C, H, W]: ", X.shape)
-        #     print("Shape of y: ", y.shape, y.dtype)
-        #     break
+    # breakpoint()
 
-        # Print model
-        model = NeuralNetwork().to(device)
-        # print(model)
+    # Display info about the shape of the data loaders
+    for X, y in test_dataloader:
+        print("Shape of X [N, C, H, W]: ", X.shape)
+        print("Shape of y: ", y.shape, y.dtype)
+        break
 
-        # Define loss function and optimizer
-        loss_fn = nn.CrossEntropyLoss()
-        optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    # Print model
+    model = NeuralNetwork().to(device)
+    # model = model.cuda()
+    print(model)
 
-        # Run model for 25 epochs
-        epochs = 3
-        for t in range(epochs):
-            # print(f"Epoch {t+1}\n-------------------------------")
-            train(train_dataloader, model, loss_fn, optimizer)
-            correct, test_loss = test(test_dataloader, model, loss_fn)
+    # Define loss function and optimizer
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-            if t == epochs - 1:
-                print_str = f"Test Error - [Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}] "
-                print(print_str)
-                f.write(print_str)
+    # Run model for 25 epochs
+    epochs = 3
 
-        time_str = f"Time taken: {time.time() - start_time} \n"
-        print(time_str)
-        f.write(time_str)
+    for t in range(epochs):
+
+        # print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer)
+        correct, test_loss = test(test_dataloader, model, loss_fn)
+
+        if t == epochs - 1:
+            print_str = f"Test Error - [Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}] "
+            print(print_str)
+            f.write(print_str)
+
+    time_str = f"Time taken: {time.time() - start_time} \n"
+    print(time_str)
+    f.write(time_str)
 
 
 print("Done!")
