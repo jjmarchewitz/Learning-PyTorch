6a5a2c6fe7b680430b433bafe5d9e4c185f6a0d2
Jake Marchewitz <7871951+jjmarchewitz@users.noreply.github.com>
2021-12-22 10:23:14 -0500

    Made updates to run_model.py

--- run_model.py
+++ run_model.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import matplotlib.pyplot as plt
 import pygame as pg
 from src.nn_class import NeuralNetwork
@@ -13,8 +14,23 @@ print(f"Using {device} device")
 
 model = NeuralNetwork().to(device)
 # Load trained model from models folder
-model.load_state_dict(
-    torch.load("models/98_percent_model.pt", map_location=torch.device(device))
-)
 
-breakpoint()
+state_dict = torch.load("models/98_percent_model.pt", map_location=torch.device(device))
+model.load_state_dict(state_dict)
+
+
+@dataclass
+class WindowProperties:
+    width: int = 500
+    height: int = 500
+
+
+pg.init()
+running = True
+window_properties = WindowProperties()
+
+while running:
+    # Initialize display surface as a window
+    display_surface = pg.display.set_mode(
+        (window_properties.width, window_properties.height)
+    )
